bf8095c311e7acc5d8458fc769233407b25c8a70
John <johnshew@twitter.com>
2016-02-16 09:56:49 -0800

    Added test server and github CNAME

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var restify = require('restify');
+
+var port = process.env.PORT || 8000;
+var server = restify.createServer();
+
+server.use(restify.gzipResponse());
+server.use(restify.queryParser());
+
+server.get(/\/public\/?.*/, restify.serveStatic({
+    directory: __dirname
+}));
+
+server.listen(port, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
